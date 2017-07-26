fca82ac24b30a07400b0cde6e8bc6ef658834fee
Scott <ssny88@gmail.com>
2017-07-26 14:13:01 -0700

    add main content panel section and render under headbar

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import MainContent from './MainContent';
 
 class App extends React.Component {
 
@@ -7,6 +8,7 @@ class App extends React.Component {
     return(
       <div className="site-wrapper">
         <Header />
+        <MainContent />
       </div>);
   }
 }
---
+++ js/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainContent extends React.Component {
+
+  render() {
+    return(
+      <div className="delivery-options-page">
+        <div className="site-container">
+          <div className="delivery-options-box">
+            test
+          </div>
+        </div>
+      </div>
+  );
+  }
+}
+
+export default MainContent;
