4b311507db9eaf4dbfcacbd09ce4cb5269d0d530
Scott <ssny88@gmail.com>
2017-07-26 11:57:50 -0700

    render header component in main app

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
 class App extends React.Component {
 
   render() {
-    return(<h1>Hello App</h1>);
+    return(
+      <div className="site-wrapper">
+        <Header />
+      </div>);
   }
 }
 
