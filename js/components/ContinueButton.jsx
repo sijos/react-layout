74588625197cedc1af7f47ca143e0a5910aeaaf9
Scott <ssny88@gmail.com>
2017-07-26 16:18:07 -0700

    refactor and clean up render method

--- js/components/ContinueButton.jsx
+++ js/components/ContinueButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class ContinueButton extends React.Component {
 
   render() {
-    const btnId = this.props.btnId
+    const btnId = this.props.btnId;
     return(
       <div className="continue-button-section" id={`continueButtonSec${btnId}`}>
         <a className="btn btn-primary" href="#" role="button">Continue</a>
