0dea91799d882bdbc360ccd6c79424f9c9410cb7
Scott <ssny88@gmail.com>
2017-07-26 11:51:57 -0700

    initial commit header component

---
+++ js/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <div className="header">
+            <div className="header-container">
+              <div className="header-resp-back">
+                  <a href="#"><img src="images/chevron-left.png" border="0" /></a>
+              </div>
+              <div className="logo"><img src="images/logo.png" /></div>
+              <div className="main-navigation">
+              <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Mac</a></li>
+              <li><a href="#">iPhone</a></li>
+              <li><a href="#">iPad</a></li>
+              <li><a href="#">Watch</a></li>
+              <li><a href="#">Support</a></li>
+              </ul>
+              </div>
+              <div className="clear"></div>
+            </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
