9edefb09b1202af105b9810a277fca24ceae2348
Scott <ssny88@gmail.com>
2017-07-27 16:02:42 -0700

    remove console.log for testing

--- js/components/ContinueButton.jsx
+++ js/components/ContinueButton.jsx
@@ -5,7 +5,6 @@ class ContinueButton extends React.Component {
   render() {
     const btnId = this.props.btnId;
     const dispType = this.props.hidden ? "none" : "inherit";
-    console.log(dispType);
     return(
       <div className="continue-button-section" id={`continueButtonSec${btnId}`} style={{display:`${dispType}`}}>
         <a className="btn btn-primary" href="#" role="button">Continue</a>
