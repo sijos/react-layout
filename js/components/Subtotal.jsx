30953d73972e26768f60fa7d0d0305a48e9297ba
Scott <ssny88@gmail.com>
2017-07-31 12:02:46 -0700

    update className attrs

--- js/components/Subtotal.jsx
+++ js/components/Subtotal.jsx
@@ -4,38 +4,38 @@ class Subtotal extends React.Component {
 
   render() {
     return (
-      <div class="internal-pad-1">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="email-recycling-options"><input type="checkbox" name="recycling_options" /> &nbsp; Yes, email me about recycling options</div>
+      <div className="internal-pad-1">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="email-recycling-options"><input type="checkbox" name="recycling_options" /> &nbsp; Yes, email me about recycling options</div>
           </div>
-          <div class="col-sm-6">
-            <div class="total-pricing-sec">
-              <div class="row">
-                <div class="col-sm-6 tabletresp1">&nbsp;</div>
-                <div class="col-sm-6 tabletresp2">
-                  <div class="row">
-                    <div class="col-sm-6 resp">
+          <div className="col-sm-6">
+            <div className="total-pricing-sec">
+              <div className="row">
+                <div className="col-sm-6 tabletresp1">&nbsp;</div>
+                <div className="col-sm-6 tabletresp2">
+                  <div className="row">
+                    <div className="col-sm-6 resp">
                       <h5>Bag Subtotal</h5>
                     </div>
-                    <div class="col-sm-6 resp">
-                      <div class="price">$769.00</div>
+                    <div className="col-sm-6 resp">
+                      <div className="price">$769.00</div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-sm-6 botbor resp">
-                      <h5 class="green">Free Shipping</h5>
+                  <div className="row">
+                    <div className="col-sm-6 botbor resp">
+                      <h5 className="green">Free Shipping</h5>
                     </div>
-                    <div class="col-sm-6 botbor resp">
-                      <div class="price green">$0.00</div>
+                    <div className="col-sm-6 botbor resp">
+                      <div className="price green">$0.00</div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-sm-6 resp">
+                  <div className="row">
+                    <div className="col-sm-6 resp">
                       <h4>Order Total</h4>
                     </div>
-                    <div class="col-sm-6 resp">
-                      <div class="total-price">$769.00</div>
+                    <div className="col-sm-6 resp">
+                      <div className="total-price">$769.00</div>
                     </div>
                   </div>
                 </div>
@@ -43,14 +43,14 @@ class Subtotal extends React.Component {
             </div>
           </div>
         </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="enter-promocode">
-            <div class="enter-promo-field-div" id="promoFieldDiv" style="display:none;">
-                <div class="form-fields">
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <div class="form-group">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="enter-promocode">
+            <div className="enter-promo-field-div" id="promoFieldDiv" style="display:none;">
+                <div className="form-fields">
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <div className="form-group">
                         <input name="promo-code" type="text" id="" placeholder="Promo Code" /> &nbsp;
                         <a href="#">Apply</a>
                       </div>
